aa10a8eac286c31fca418a0ac6ea8bf51aa1e6a5
Sumit Rawat <sumitrawat9910@gmail.com>
2025-08-07 18:05:07 +0530

    [ui][fix]: provided appropriate vertical padding to flatlist's content container

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,21 @@ import ActionBar from "../components/ActionBar/ActionBar";
 import Header from "../components/Header";
 
 import { FlatList, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 import Animated, {
   useAnimatedKeyboard,
   useAnimatedStyle,
 } from "react-native-reanimated";
 
+const PADDING_FROM_EDGES = 20;
+
 function Index() {
+  const { top, bottom } = useSafeAreaInsets();
+
+  const HEADER_HEIGHT = 50 + PADDING_FROM_EDGES + top;
+  const ACTION_BAR_HEIGHT = 40 + PADDING_FROM_EDGES + bottom; // //not final;
+
   const data = Array.from({ length: 100 }, (_, i) => i + 1);
 
   const keyboard = useAnimatedKeyboard();
@@ -23,7 +32,11 @@ function Index() {
         <FlatList
           keyboardDismissMode="on-drag"
           data={data}
-          contentContainerStyle={{ gap: 10 }}
+          contentContainerStyle={{
+            gap: 10,
+            paddingBottom: ACTION_BAR_HEIGHT,
+            paddingTop: HEADER_HEIGHT,
+          }}
           renderItem={({ item }) => (
             <View style={{ width: 100, height: 100, backgroundColor: "red" }} />
           )}
